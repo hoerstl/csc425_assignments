958c51c435b374da0789bcad56457d89468c071e
hoerstl <hoerstl@csc4252022.ssdt.berea.edu>
2023-09-24 16:41:47 -0400

    Added a print statement for the process id

--- coding.7/memory-user.c
+++ coding.7/memory-user.c
@@ -10,14 +10,15 @@ int main(int argc, char **argv){
 
 long long bytesToUse = atoi(argv[1]) * 1024 * 1024;
 int* timeWaster = malloc(bytesToUse);
-int numbersInArray = bytesToUse / sizeof(int);
+long long numbersInArray = bytesToUse / (long long)sizeof(int);
+printf("Here's the pid for convenience: %d\n", getpid());
 printf("Tried to allocate %lld bytes\n", bytesToUse);
-printf("%d integers will fit in the allocated array\n", numbersInArray);
+printf("%lld integers will fit in the allocated array\n", numbersInArray);
 
-int i = 0;
+long long i = 0;
 while(1){
 
-timeWaster[i] = 1;
+    timeWaster[i] = 1;
     i = (i + 1) % numbersInArray;
 }
 
